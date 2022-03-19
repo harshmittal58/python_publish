b95644eb096b09c0f438b4c00413addff105e157
Harsh mittal <harsh.mittal@decimal.co.in>
2022-03-19 10:41:27 +0530

    new label making

--- pythonpg.py
+++ pythonpg.py
@@ -1,6 +1,7 @@
 from tkinter import *
 root=Tk()
 Label(root,text="hello codeworked").pack()
+Label(root,text="hello harsh").pack()
 Button(root,text="exit",command=lambda:root.destroy()).pack()
 
 root.title("my application")
